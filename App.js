2c9039c1acb7e6824c6bd6ebd3543922cde06f3b
Jordi <ofineo@gmail.com>
2018-06-16 12:24:29 +0100

    show a button on the screen

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import FetchLocation from './components/FetchLocation';
 
 export default class App extends React.Component {
+
+  GetUserLocationHandler =()=>{
+    console.log('Pressed button');
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <FetchLocation
+        onGetLocation={this.GetUserLocationHandler}
+        />
       </View>
     );
   }
